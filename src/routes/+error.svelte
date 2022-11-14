9a44fcc9812aeba9a922ff0cf3851ee16ce2c8bc
Max Seelig <maxseelig@gmail.com>
2022-11-14 01:30:53 +0100

    Error fix

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,9 +4,5 @@ import { page } from '$app/stores'
 </script>
 
 <h2>{$page.status}</h2>
-<p class="subhead">{$page.error.message}</p>
 
-<p><strong>Sorry!</strong> Maybe try one of these links?</p>
-<ul>
-	<li><a href="/">Home</a></li>
-</ul>
+<p><strong>Sorry!</strong> Deze pagina bestaat niet (meer).</p>
