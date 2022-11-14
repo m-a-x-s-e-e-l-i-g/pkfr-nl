1ff079edaf6220a5cd410bb4266f586ed85258b0
Max Seelig <maxseelig@gmail.com>
2022-11-14 23:40:48 +0100

    Added Parkour Culture YouTube

--- src/routes/community/+page.svelte
+++ src/routes/community/+page.svelte
@@ -4,6 +4,7 @@
 	import whatsapp from 'svelte-awesome/icons/whatsapp';
 	import comment from 'svelte-awesome/icons/comment';
 	import instagram from 'svelte-awesome/icons/instagram';
+	import youtube from 'svelte-awesome/icons/youtube';
 
 </script>
 
@@ -31,5 +32,6 @@
 	<li><span><Icon data={whatsapp}/></span><a href="https://chat.whatsapp.com/IN54zbsYPwQKAyXedO9xJB" rel="noreferrer" target="_blank">Groepschat om te kletsen</a></li>
 	<li><span><Icon data={comment}/></span><a href="https://discord.com/invite/FeECDvG" rel="noreferrer" target="_blank">Discord om te chillen</a></li>
 	<li><span><Icon data={instagram}/></span><a href="https://www.instagram.com/parkour_culture_NL/" rel="noreferrer" target="_blank">Parkour Culture Instagram</a></li>
+	<li><span><Icon data={youtube}/></span><a href="https://www.youtube.com/channel/UCqpV0z_uw6aaLVXIW6wh21g" rel="noreferrer" target="_blank">Parkour Culture YouTube</a></li>
 
 </ul>
