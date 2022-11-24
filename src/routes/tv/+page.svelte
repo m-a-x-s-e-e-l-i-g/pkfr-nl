5a9efb476946b55358376bc51d798ac16c3ad37d
Max Seelig <maxseelig@gmail.com>
2022-11-24 16:38:33 +0100

    Hotfix for TV autoplay

--- src/routes/tv/+page.svelte
+++ src/routes/tv/+page.svelte
@@ -4,7 +4,7 @@
 	import { page } from '$app/stores';
 
 	let autoplayParam = '';
-	if(import.meta.env.VITE_TELEGRAM_BOT_API_KEY === true){
+	if(import.meta.env.VITE_YOUTUBE_AUTOPLAY === true){
 		autoplayParam = '&autoplay=1';
 	}
 </script>
