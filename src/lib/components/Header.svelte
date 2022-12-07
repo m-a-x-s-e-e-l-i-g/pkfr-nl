341cf56db3a0f433ed7705a17ac6248b01580435
Max Seelig <maxseelig@gmail.com>
2022-12-07 03:12:44 +0100

    yeet

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,19 +2,11 @@
 	import MainNav from './MainNav.svelte'
 	import HamburgerMenuButton from './HamburgerMenuButton.svelte'
 	import { siteTitle } from '$lib/config'
-
-	const focusMain = () => {
-		const main = document.querySelector('main');
-		main.focus();
-	}
 </script>
 
 
 <header>
-	<a on:click|preventDefault={focusMain} class="skip-to-content-link" href="#main">
-		Skip to main content
-	</a>
-	
+
 	<a href="/" class="site-title">
 		{siteTitle}
 	</a>
