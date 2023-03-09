2b1fc30361a6dea434d6378fe8601a041d5d7316
Max Seelig <maxseelig@gmail.com>
2023-03-09 16:31:42 +0100

    Added spotify playlist and parkourculture website

--- src/routes/community/+page.svelte
+++ src/routes/community/+page.svelte
@@ -5,6 +5,8 @@
 	import comment from 'svelte-awesome/icons/comment';
 	import instagram from 'svelte-awesome/icons/instagram';
 	import youtube from 'svelte-awesome/icons/youtube';
+	import spotify from 'svelte-awesome/icons/spotify';
+	import externalLink from 'svelte-awesome/icons/externalLink';
 
 </script>
 
@@ -28,9 +30,11 @@
 <h1>Community Links</h1>
 <ul>
 	<!-- whatsapp icon -->
-	<li><span><Icon data={whatsapp}/></span><a href="https://chat.whatsapp.com/GamwcVgPYnq3UbKog2H9M8" rel="noreferrer" target="_blank">Whatsapp Community (Parkour Culture)</a></li>
+	<li><span><Icon data={whatsapp}/></span><a href="https://chat.whatsapp.com/GamwcVgPYnq3UbKog2H9M8" rel="noreferrer" target="_blank">Whatsapp Community (Parkourculture)</a></li>
 	<li><span><Icon data={comment}/></span><a href="https://discord.com/invite/FeECDvG" rel="noreferrer" target="_blank">Discord om te chillen</a></li>
-	<li><span><Icon data={instagram}/></span><a href="https://www.instagram.com/parkour_culture_NL/" rel="noreferrer" target="_blank">Instagram (Parkour Culture)</a></li>
-	<li><span><Icon data={youtube}/></span><a href="https://www.youtube.com/channel/UCqpV0z_uw6aaLVXIW6wh21g" rel="noreferrer" target="_blank">YouTube (Parkour Culture)</a></li>
+	<li><span><Icon data={instagram}/></span><a href="https://www.instagram.com/parkour_culture_NL/" rel="noreferrer" target="_blank">Instagram (Parkourculture)</a></li>
+	<li><span><Icon data={youtube}/></span><a href="https://www.youtube.com/channel/UCqpV0z_uw6aaLVXIW6wh21g" rel="noreferrer" target="_blank">YouTube (Parkourculture)</a></li>
+	<li><span><Icon data={spotify}/></span><a href="https://spoti.fi/3OazXTm" rel="noreferrer" target="_blank">Spotify playlist</a></li>
+	<li><span><Icon data={externalLink}/></span><a href="https://parkourculture.nl/" rel="noreferrer" target="_blank">Parkourculture website (voor lessen, workshops en kinderfeesten in Utrecht en omstreken)</a></li>
 
 </ul>
