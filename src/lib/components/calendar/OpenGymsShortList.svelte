e69023966da8d3e6af90e1694a950dd63a0bde49
Max Seelig <maxseelig@gmail.com>
2023-06-22 18:28:11 +0200

    Added open gyms to home page

---
+++ src/lib/components/calendar/OpenGymsShortList.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import FullCalendar, { type CalendarOptions } from 'svelte-fullcalendar';
+	import ListPlugin from '@fullcalendar/list';
+	import googleCalendarPlugin from '@fullcalendar/google-calendar';
+	import nlLocale from '@fullcalendar/core/locales/nl';
+
+  let options: CalendarOptions = {
+		locale: nlLocale,
+		initialView: 'list',
+		headerToolbar: false,
+		views: {
+			list: {
+				duration: { hours: 72 },
+			}
+		},
+		plugins: [ListPlugin, googleCalendarPlugin],
+		googleCalendarApiKey: import.meta.env.VITE_GOOGLE_API_KEY,
+		events: {
+			googleCalendarId: import.meta.env.VITE_OPEN_GYM_CALENDAR_ID
+		},
+		eventMouseEnter: (info) => {
+			if (info.event.extendedProps.description) {
+				info.el.setAttribute('title', info.event.extendedProps.description.replace(/(<([^>]+)>)/gi, ''));
+			}
+		},
+		eventClick: (info) => {
+			if (info.event.url) {
+				info.jsEvent.preventDefault();
+				window.open(info.event.url, '_blank');
+			}
+		},
+	};
+</script>
+
+<FullCalendar {options} />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import OpenGymsShortList from '$lib/components/calendar/OpenGymsShortList.svelte';
 	import { siteTitle, titlePostfix } from '$lib/config';
 </script>
 
@@ -26,12 +27,14 @@
 {siteTitle} is dé online hub voor de Nederlandse parkour en freerunning community. Hier vind je alles wat nodig is om deel uit te maken van deze fantastische community en op de hoogte te blijven van wat er gebeurt.
 </p>
 
+<h2>Aankomenende Open Gyms</h2>
+<OpenGymsShortList />
+<a
+	href="/open-gyms"
+	class="button">Bekijk alle open gyms</a
+>
+
 <h2>Over {siteTitle}</h2>
 <p>
 	Het doel van {siteTitle} is om alle handige informatie over freerunning en parkour op één plek te hebben. Hier zijn overzichten van de <a href="/spots">beste spots</a>, <a href="/open-gyms">open gyms</a>, <a href="/community">community linkjes</a> en een <a href="/tv">playlist met Nederlandse freerun videos</a>. Daarnaast is er een <a href="/blog">blog</a> waar nieuwtjes binnen de Nederlandse parkour en freerunning community worden gedeeld.
-</p>
-
-<h2>Missie</h2>
-<p>
-	{siteTitle} gelooft dat parkour en freerunning voor iedereen toegankelijk moet zijn. Of je nu een beginnende of ervaren freerunner bent, {siteTitle} helpt om deel uit te maken van de Nederlandse parkour en freerunning community. De website is een handige tool voor iedereen die op de hoogte wil blijven van wat er gebeurt binnen de Nederlandse parkour en freerunning community.
 </p>
