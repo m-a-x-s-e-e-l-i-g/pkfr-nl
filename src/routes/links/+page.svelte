cc251a82a6283862fa5c4e620943bfb3103b7314
Max Seelig <7907436+m-a-x-s-e-e-l-i-g@users.noreply.github.com>
2023-09-20 00:45:48 +0200

    Removed YouTube from links

--- src/routes/links/+page.svelte
+++ src/routes/links/+page.svelte
@@ -2,9 +2,7 @@
 	import { titlePostfix } from '$lib/config';
 	import Icon from 'svelte-awesome';
 	import whatsapp from 'svelte-awesome/icons/whatsapp';
-	import comment from 'svelte-awesome/icons/comment';
 	import instagram from 'svelte-awesome/icons/instagram';
-	import youtube from 'svelte-awesome/icons/youtube';
 	import spotify from 'svelte-awesome/icons/spotify';
 	import externalLink from 'svelte-awesome/icons/externalLink';
 
@@ -31,7 +29,6 @@
 <ul>
 	<li><span><Icon data={whatsapp}/></span><a href="https://chat.whatsapp.com/GamwcVgPYnq3UbKog2H9M8" rel="noreferrer" target="_blank">Whatsapp Community (Parkourculture)</a></li>
 	<li><span><Icon data={instagram}/></span><a href="https://www.instagram.com/parkour_culture_NL/" rel="noreferrer" target="_blank">Instagram (Parkourculture)</a></li>
-	<li><span><Icon data={youtube}/></span><a href="https://www.youtube.com/channel/UCqpV0z_uw6aaLVXIW6wh21g" rel="noreferrer" target="_blank">YouTube (Parkourculture)</a></li>
 	<li><span><Icon data={spotify}/></span><a href="https://spoti.fi/3OazXTm" rel="noreferrer" target="_blank">Spotify playlist</a></li>
 	<li><span><Icon data={externalLink}/></span><a href="https://parkourculture.nl/" rel="noreferrer" target="_blank">Parkourculture website (voor lessen, workshops en kinderfeesten in Utrecht en omstreken)</a></li>
 </ul>
