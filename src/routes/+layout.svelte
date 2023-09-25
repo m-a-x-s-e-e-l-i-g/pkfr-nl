5bf2d650b01bf60316062d1df209b78e54f529d1
Max Seelig <maxseelig@gmail.com>
2023-09-25 23:32:40 +0200

    Removed prerender constant that wouldve been ignored anyways

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,8 +13,6 @@
 	const transitionIn = { delay: 150, duration: 150 }
 	const transitionOut = { duration: 100 }
 
-  export const prerender = true
-	
 	/**
 	 * Updates the global store with the current path. (Used for highlighting 
 	 * the current page in the nav, but could be useful for other purposes.)
