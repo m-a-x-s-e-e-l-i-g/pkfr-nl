a108c20d24bf3216016b053e2e3cfb6ec83ebf35
Max Seelig <7907436+m-a-x-s-e-e-l-i-g@users.noreply.github.com>
2024-03-10 01:33:23 +0100

    More gym info

--- src/routes/gym-finder/+page.svelte
+++ src/routes/gym-finder/+page.svelte
@@ -21,11 +21,19 @@
 	<title>Gym Finder {titlePostfix}</title>
 </svelte:head>
 
-
 <h1>Gym Finder</h1>
-<p>Hier vind je gyms!</p>
-<ul>
+<p>Hier vind je gyms! Sta je locatie toe om dichtsbijzijnde gyms te zien.</p>
+<div class="flex flex-col space-y-4">
 	{#each getGyms(latitude, longitude) as gym}
-		<li>{gym.name}</li>
+		<div class="p-6 bg-white rounded-xl shadow-md">
+			<h2 class="text-2xl font-bold text-black my-0 mb-4">{gym.name}</h2>
+			<p class="text-gray-500">📍 <a href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(gym.address)}`} target="_blank" rel="noopener noreferrer">{gym.address}</a></p>
+			<p class="text-gray-500">🌐 <a href={gym.website} class="text-blue-500">{gym.website}</a></p>
+			<div>
+				{#each gym.images as image}
+					<img src={image} alt={gym.name} class="w-full h-64 object-cover mt-2 rounded"/>
+				{/each}
+			</div>
+		</div>
 	{/each}
-</ul>
+</div>
