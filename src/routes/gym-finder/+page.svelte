a0dee95e25dba173d5bbaefe09b9414f21e53045
Max Seelig <7907436+m-a-x-s-e-e-l-i-g@users.noreply.github.com>
2024-03-10 23:20:17 +0100

    Add alert components

--- src/routes/gym-finder/+page.svelte
+++ src/routes/gym-finder/+page.svelte
@@ -4,9 +4,12 @@
 	import { getGyms } from '$lib/assets/js/gyms.js';
 	import Autoplay from "embla-carousel-autoplay";
 	import * as Carousel from "$lib/components/ui/carousel/index.js";
+	import * as Alert from "$lib/components/ui/alert/index.js";
+	import Icon from 'svelte-awesome';
+	import locationArrow from 'svelte-awesome/icons/locationArrow';
 
-	var latitude = 0;
-	var longitude = 0;
+	var latitude;
+	var longitude;
 
 	onMount(() => {
 		// retrieve user location
@@ -22,19 +25,27 @@
 	<title>Gym Finder {titlePostfix}</title>
 </svelte:head>
 <h1>Gym Finder</h1>
-<p>Hier vind je gyms! Sta je locatie toe om dichtsbijzijnde gyms te zien.</p>
-<div class="grid w-full max-w-sm items-center gap-1.5 mb-8 mt-8">
-	<p>Aantal resultaten: {getGyms(latitude, longitude).length}</p>
-</div>
+<p>Hier vind je <b>{getGyms(latitude, longitude).length}</b> indoor <b>gyms</b>!</p>
+{#if !latitude && !longitude}
+	<Alert.Root>
+		<Icon data={locationArrow} class="h-4 w-4"/>
+		<Alert.Title>Geef pkfr.nl locatierechten.</Alert.Title>
+		<Alert.Description>
+			Deel je locatie om de lijst te sorteren op afstand.
+		</Alert.Description>
+	</Alert.Root>
+{/if}
 <div class="flex flex-col space-y-4">
 	{#each getGyms(latitude, longitude) as gym}
 		<div class="p-6 bg-white rounded-xl shadow-md relative">
 			<h2 class="text-2xl font-bold text-black my-0 mb-4">
 				{gym.name}
 			</h2>
-			<small class="absolute top-4 right-4 font-bold">
-				{gym.distance.toFixed(1)} km
-			</small>			
+			{#if latitude && longitude}
+				<small class="absolute top-4 right-4 font-bold">
+					{gym.distance.toFixed(1)} km
+				</small>			
+			{/if}
 			<p class="text-gray-500">📍 <a href={`https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(gym.address)}`} target="_blank" rel="noopener noreferrer">{gym.address}</a></p>
 			<p class="text-gray-500">🌐 <a href={gym.website} class="text-blue-500">{gym.website}</a></p>
 			<Carousel.Root
