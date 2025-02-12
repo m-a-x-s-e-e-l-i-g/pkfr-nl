c67e1b4ad2901f2a5f8aa61c47e5b02aafabadc6
Max Seelig <maxseelig@gmail.com>
2025-02-12 13:15:28 +0100

    Fixes for Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,24 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
-	resolve: {
-		dedupe: ['@fullcalendar/common'],
-	},
-	optimizeDeps: {
-		include: ['@fullcalendar/common'],
-	},
-	server: {
-		watch: {
-			usePolling: true,
-			interval: 50,
-		},
-		fs: {
-			allow: ['.']
-		}
-	}
+    plugins: [tailwindcss(), sveltekit()],
+    resolve: {
+        dedupe: ['@fullcalendar/common']
+    },
+    optimizeDeps: {
+        include: ['@fullcalendar/common']
+    },
+    server: {
+        watch: {
+            usePolling: true,
+            interval: 50
+        },
+        fs: {
+            allow: ['.']
+        }
+    }
 };
 
 export default config
