f9a32f97d41196a325277c620d78117a6962cbe4
Max Seelig <maxseelig@gmail.com>
2025-07-13 13:44:05 +0200

    Make buttons scale better on mobile Fixes #398

---
+++ src/lib/components/svg/DownloadSVG.svelte
@@ -0,0 +1,23 @@
+<svg 
+  width="20" 
+  height="20" 
+  viewBox="0 0 24 24" 
+  fill="none" 
+  xmlns="http://www.w3.org/2000/svg"
+  class="icon"
+>
+  <path 
+    d="M21 15V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V15M7 10L12 15L17 10M12 15V3" 
+    stroke="currentColor" 
+    stroke-width="2" 
+    stroke-linecap="round" 
+    stroke-linejoin="round"
+  />
+</svg>
+
+<style>
+  .icon {
+    display: inline-block;
+    vertical-align: middle;
+  }
+</style>
