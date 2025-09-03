8be3d9f8c0a621943a69b32973b19c8c7a5f503c
Max Seelig <maxseelig@gmail.com>
2025-09-03 15:17:41 +0200

    Add placelist entry for Göttingen, Kassel, Hannover, Hamburg

--- src/lib/components/PlacelistFilter.svelte
+++ src/lib/components/PlacelistFilter.svelte
@@ -235,7 +235,8 @@
 	{ name: "FRBodensee Festival", url: "https://maps.app.goo.gl/1a8aCea1s2rN6Z5H8?g_st=ac", country: "Germany", author: "Jonas Metzger" },
 	{ name: "FreeGrunn Veendam", url: "https://maps.app.goo.gl/hxiQqciZGPzAY5Lb7?g_st=ic", country: "Netherlands", author: "Flexbeweging" }, 
 	{ name: "FreeGrunn Groningen", url: "https://www.google.com/maps/d/edit?mid=1r00q3rS8Bta2sn_90vZsL-j8sO7MD9c&usp=share_link", country: "Netherlands", author: "Lennard Koops" },
-	{ name: "Göttingen Z Campus Mini Spots", url: "https://maps.app.goo.gl/M5HRYycDGK6tZM3G8", country: "Germany", author: "Lennart Jakob" }
+	{ name: "Göttingen Z Campus Mini Spots", url: "https://maps.app.goo.gl/M5HRYycDGK6tZM3G8", country: "Germany", author: "Lennart Jakob" },
+	{ name: "Göttingen, Kassel, Hannover, Hamburg", url: "https://maps.app.goo.gl/Jh4271PCrVZCbqvp6?g_st=iw", country: "Germany", author: "Lennart Jakob" }
 ];
 
   // Sort by name for better UX
