9ff126579a5e52ecd636adec143a1cfcf716b185
Max Seelig <maxseelig@gmail.com>
2025-09-10 21:22:59 +0200

    feat: Seperate movies and playlists data

---
+++ src/lib/assets/playlists.ts
@@ -0,0 +1,32 @@
+export const playlists = [
+	{
+		id: 1,
+		title: "Dutch Community TV",
+		creator: "the Netherlands",
+		description: "Dutch parkour content from the community.",
+		thumbnail: "/images/posters/dutch-parkour-community-tv-playlist-freerunning.webp",
+		playlistId: "PL3iwaCsp8s8P0lVvw3DkmMFmGxqBGSXnS",
+		type: "playlist",
+		videoCount: 53
+	},
+	// {
+	// 	id: 2,
+	// 	title: "Must Watch: De Ultieme Playlist",
+	// 	creator: "Koen Bangert",
+	// 	description: "The ultimate international parkour must-watch collection.",
+	// 	thumbnail: "",
+	// 	playlistId: "PLIZ9EYUk7YaYgqhOHd1MqROfTmNw79UBy",
+	// 	type: "playlist",
+	// 	videoCount: 125
+	// },
+	{
+		id: 3,
+		title: "Parkour - The Nature of Challenge",
+		creator: "Northern Parkour",
+		description: "A short documentary series exploring what parkour means to practitioners and the so-called 'parkour vision'.",
+		thumbnail: "/images/posters/parkour-the-nature-of-challenge-poster.webp",
+		playlistId: "PLFE063C4ADA6CAA18",
+		type: "playlist",
+		videoCount: 4
+	}
+];
